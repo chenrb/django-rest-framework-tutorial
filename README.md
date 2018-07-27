380d0473efd55d94ba630ee3d914d7000c303d95
Rubi <rubi@canway.net>
2018-07-27 18:21:00 +0800

    '请求与响应'

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # django-rest-framework-tutorial
 
-第一张： 序列化
+第二章： 请求与响应
+使用`@api_view(['GET', 'POST'])`装饰视图函数
+使用`from rest_framework.urlpatterns import format_suffix_patterns`为url添加后缀，如`.json/.api`
--- drft/snippets/views.py
+++ drft/snippets/views.py
@@ -1,63 +1,60 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from snippets.models import Snippets
 from snippets.serializers import SnippetSerializer
 
 
-# Create your views here.
+# URL添加后缀格式, 通过在视图函数中添加一个format参数,urls.py添加format_suffix_patterns
 
-class JSONResponse(HttpResponse):
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
     """
-    An HttpResponse that renders its content into JSON.
+    List all, or create new
+    :param request:
+    :return:
     """
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@csrf_exempt
-def snippet_list(request):
     if request.method == 'GET':
         snippets = Snippets.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+    if request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete
+    :param request:
+    :return:
+    """
     try:
         snippet = Snippets.objects.get(pk=pk)
     except Snippets.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
