96eaef7f69bb5fe045094412a7686e8d8eb9b46a
chenrb <crbxyz@foxmail.com>
2020-03-01 09:14:07 +0800

    feature: 准备好教程工程

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+API_TITLE = "Pastebin API"
+API_DESCRIPTION = "A Web API for creating and viewing highlighted code snippets."
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("snippets.urls")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("schema/", schema_view),
+    path("docs/", include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
 ]
